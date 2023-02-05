1c99e82a6791ebf7c45ffdc9fa1e39ddc85a0adc
Faizal Patel <faizalpatel@Faizals-MacBook-Air.local>
2023-02-05 13:23:16 -0600

    drop down select added to be able to pick different genres of music

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,28 @@
-const Discover = () => <div>Discover</div>;
+// imports
+//components are divs that say component name
+import {Error, Loader, SongCard} from '../components';
+import{genres} from '../assets/constants';
+
+
+const Discover = () => {
+    //console logg
+    console.log(genres);
+
+
+    return(
+        <div className='flex flex-col'>
+
+            <div className='w-full flex justify-between items-center sm:flex-row
+            flex-col mt-4 mb-10'>
+
+                <select>
+                    
+                </select>
+
+            </div>
+
+        </div>
+    );
+};
 
 export default Discover;
