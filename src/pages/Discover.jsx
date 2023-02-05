82f66678e3485119a781192baeeba0c6baf39d83
Faizal Patel <faizalpatel@Faizals-MacBook-Air.local>
2023-02-05 13:26:59 -0600

    added different genres to the select menu

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -8,6 +8,9 @@ const Discover = () => {
     //console logg
     console.log(genres);
 
+    //hardcoding genre title
+    const genreTitle = 'Electric';
+
 
     return(
         <div className='flex flex-col'>
@@ -15,8 +18,14 @@ const Discover = () => {
             <div className='w-full flex justify-between items-center sm:flex-row
             flex-col mt-4 mb-10'>
 
-                <select>
-                    
+                <h2 className='font-bold text-3xl text-red-100 text-left'>Discover {genreTitle}</h2>
+                {/* genre select */}
+                <select
+                    onChange={()    =>  {}}
+                    value = ''
+                    className='bg-black text-gray-200 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
+                >
+                    {genres.map((genre) =>  <option>{genre.title}</option>)}
                 </select>
 
             </div>
