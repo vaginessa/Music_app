61fd750c5389d61a205f1166a2feda1e2a170ef8
Faizal Patel <faizalpatel@Faizals-MacBook-Air.local>
2023-02-07 13:56:34 -0600

    implemented searchbar functionality so users can actually type in it. Used handle submit

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,8 +5,18 @@ import { useNavigate } from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
 
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-300 focus-within:text-gray-500">
+const Searchbar = () => {
+
+  const navigate = useNavigate(); 
+  const [searchTerm, setSearchTerm] = useState(''); 
+
+  const handleSubmit = (event)  => {
+    event.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  }
+return (
+  <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-300 focus-within:text-gray-500">
 
     <label htmlFor="search-field" className="sr-only">Searching all the songs</label>
 
@@ -20,13 +30,15 @@ const Searchbar = () => (
         placeholder='Search Here'
         id = "search-field"
         type = 'search'
-        value = ''
-        onChange={()  =>  {}}
+        value = {searchTerm}
+        onChange={(event)  =>  setSearchTerm(event.target.value)}
         className = 'flex-1 bg-transparent border-none outline-none placeholder-gray-400 text-base text-red-50 p-4'
       />
 
     </div>
+  
   </form>
-);
+)
+}
 
 export default Searchbar;
